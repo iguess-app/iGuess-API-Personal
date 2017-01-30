9d274189e89be34ae54428184161a9512d1fa4d7
Lucas Alves <lucashalves@live.com>
2017-01-30 00:22:00 -0200

    Added a password utils with bcrypt module system.

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -1,22 +1,47 @@
 'use Strict';
 
-module.exports = () => {
-  const checkPasswordRestrict = (password) => new Promise((resolve, reject) => {
-    //Make a regex to password restrict 
-    resolve(password)
-  })
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-  const cryptPassword = (password) => new Promise((resolve, reject) => {
+module.exports = (app) => {
+  const userErrors = app.src.utils.errorUtils.userErrors;
 
-  })
+  const checkPasswordRestrict = (userData) =>
+    new Promise((resolve, reject) => {
+      const passwordRegex = new RegExp(/(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,25}/);
+      if (passwordRegex.test(userData.password)) {
+        resolve(userData)
+      }
 
-  const checkPassword = (password) => new Promise((resolve, reject) => {
+      reject(userErrors.passwordInvalid);
+    })
 
-  })
+  const cryptPassword = (userData) =>
+    new Promise((resolve, reject) => {
+      bcrypt.hash(userData.password, saltRounds, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        userData.password = hash;
+
+        resolve(userData);
+      })
+    })
+
+  const checkPassword = (password, cryptedPassword) =>
+    new Promise((resolve, reject) => {
+      bcrypt.compare(password, cryptedPassword, (err, res) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(res)
+      })
+    })
 
   return {
-    checkPasswordRestrict, 
-    cryptPassword, 
+    checkPasswordRestrict,
+    cryptPassword,
     checkPassword
   }
 }
