f0d0f3f8ffabaa8d587df9b137b7ac925dc50907
Lucas Alves <lucashalves@live.com>
2017-04-15 17:58:10 -0300

    Updating profileSchema with notifications and guessesLeague.

--- src/schemas/profileSchema.js
+++ src/schemas/profileSchema.js
@@ -1,10 +1,40 @@
 'use strict';
 
 const mongoose = require('mongoose');
-  
+
 module.exports = (app) => {
   const Schema = mongoose.Schema;
   const db = app.src.managers.mongoManager;
+
+  const notificationsSchema = new Schema({
+    description: {
+      type: String,
+      required: true
+    },
+    positiveChoice: {
+      type: String,
+      required: true
+    },
+    negativeChoice: {
+      type: String,
+      required: true
+    },
+    saw: {
+      type: Boolean,
+      required: true
+    }
+  });
+
+  const guessesLeaguesSchema = new Schema({
+    leagueName: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true
+    }
+  })
 
   const profileSchema = new Schema({
     _id: {
@@ -32,7 +62,9 @@ module.exports = (app) => {
     },
     guessesLines: {
       type: Array
-    }
+    },
+    guessesLeagues: [guessesLeaguesSchema],
+    notifications: [notificationsSchema]
   })
 
   return db.model('profiles', profileSchema);
