7109e0df6d8c11e0f7b3b69cd160c9519180b54b
Lucas Alves <lucashalves@live.com>
2017-04-16 17:53:34 -0300

    Using id-nickname function from the profileUtil on friendsService.

--- src/application/services/friendsService.js
+++ src/application/services/friendsService.js
@@ -2,17 +2,11 @@
 
 module.exports = (app) => {
   const friendsRepository = app.src.application.repositories.friendsRepository;
+  const ProfileUtils = app.src.utils.profileUtils;
 
   const search = (request, headers) =>
     friendsRepository.search(request, headers)
-    .then((usersFound) => usersFound.map((userFound) => _structureUserObj(userFound)))
-
-  const _structureUserObj = (userFound) => {
-    Reflect.set(userFound, 'nickName', userFound._id)
-    Reflect.deleteProperty(userFound, '_id');
-
-    return userFound;
-  }
+    .then((usersFound) => usersFound.map((userFound) => ProfileUtils.useIDLikeNickname(userFound)))
 
   return {
     search
