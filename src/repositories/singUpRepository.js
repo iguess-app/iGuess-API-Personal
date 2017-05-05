e102af0eafa6243531d8e95927daf6d4998f51f2
Lucas Alves <lucashalves@live.com>
2017-05-04 23:13:32 -0300

    Create a session when a user make singUp

--- src/repositories/singUpRepository.js
+++ src/repositories/singUpRepository.js
@@ -1,20 +1,34 @@
 'use Strict';
 
-const Promise = require('bluebird');
-
 module.exports = (app) => {
   const Profile = app.coincidents.Schemas.profileSchema;
+  const TokenManager = app.coincidents.Managers.tokenManager;
+  const QueryUtils = app.coincidents.Utils.queryUtils;
 
   const singUp = (dataToDB) =>
     _insertUserOnDB(dataToDB)
 
   const _insertUserOnDB = (userData) =>
-    Promise.resolve(Profile.create(userData)
-      .then(() => ({
-        singUpSuccess: true
-      }))
-      .catch((err) => err)
-    )
+    Profile.create(userData)
+    .then((info) => {
+      const token = TokenManager.generate();
+      const user = _structureUserObj(QueryUtils.makeObject(info));
+
+      return {
+        token,
+        user
+      }
+    })
+    .catch((err) => err)
+
+  const _structureUserObj = (user) => {
+    Reflect.set(user, 'nickName', user._id)
+    Reflect.deleteProperty(user, 'password');
+    Reflect.deleteProperty(user, '__v');
+    Reflect.deleteProperty(user, '_id');
+
+    return user
+  }
 
   return {
     singUp
