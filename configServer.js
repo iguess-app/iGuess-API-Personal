9fb0a18b48e5d9ec443b668c0dc00713bd8711c0
Lucas Alves <lucashalves@live.com>
2017-05-04 22:05:43 -0300

    Connecting server to port 9002 if is local.

--- configServer.js
+++ configServer.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const Hapi = require('hapi');
+const PORT_LOCAL = 9002;
+let port = 8080;
 
-module.exports = () => {
+module.exports = (app) => {
+  const config = app.coincidents.Config;
+
+  if (config.env === 'local') {
+    port = PORT_LOCAL;
+  }
+
   const server = new Hapi.Server();
   server.connection({
     host: '0.0.0.0',
-    port: 8080
+    port
   });
 
   return server;
