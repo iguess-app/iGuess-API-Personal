cd760cdcb4e5f73a9450d82de4f5ac21710893bd
Lucas Alves <lucashalves@live.com>
2017-05-05 22:41:01 -0300

    Taking off function that is not necessary anymore.

--- src/services/friendsService.js
+++ src/services/friendsService.js
@@ -2,11 +2,8 @@
 
 module.exports = (app) => {
   const friendsRepository = app.src.repositories.friendsRepository;
-  const ProfileUtils = app.coincidents.Utils.profileUtils;
 
-  const search = (request, headers) =>
-    friendsRepository.search(request, headers)
-    .then((usersFound) => usersFound.map((userFound) => ProfileUtils.useIDLikeNickname(userFound)))
+  const search = (request, headers) => friendsRepository.search(request, headers)
 
   return {
     search
