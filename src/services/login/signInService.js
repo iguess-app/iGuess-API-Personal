cd705a7060feead12193631218dcd6a2c7ce5275
Lucas Alves <lucashalves@live.com>
2017-05-07 15:31:40 -0300

    Get on login if there is notifications unreadable, setting notications on cache

--- src/services/login/signInService.js
+++ src/services/login/signInService.js
@@ -1,9 +1,26 @@
 'use Strict';
 
+const Promise = require('bluebird');
+
 module.exports = (app) => {
   const signInRepository = app.src.repositories.login.signInRepository;
+  const notificationRepository = app.src.repositories.login.notificationRepository;
+  const CacheManager = app.coincidents.Managers.cacheManager;
 
   const singIn = (query, headers) => signInRepository.singIn(query, headers)
+    .then((singInObj) => Promise.all([singInObj, notificationRepository.getNotifications(singInObj.user.id)])
+      .spread((userObj, notificationsObj) => {
+        userObj.user.thereIsUnreadableNotification = notificationsObj.notifications.some((notification) => notification.saw === false)
+        _setNotificationsOnCache(userObj.user.userName, notificationsObj)
+
+        return userObj
+      })
+    )
+
+  const _setNotificationsOnCache = (userName, notificationsObj) => {
+    const notificationsCacheKey = `${userName}'s Notifications`
+    CacheManager.set(notificationsCacheKey, notificationsObj)
+  }
 
   return {
     singIn
