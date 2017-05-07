329e931a45a68a25078dc2e15907e9c1b5b59f44
Lucas Alves <lucashalves@live.com>
2017-05-07 00:31:13 -0300

    Added profileInfo service with all treatments

--- src/repositories/profileUpdates/profileInfoRepository.js
+++ src/repositories/profileUpdates/profileInfoRepository.js
@@ -1,16 +1,39 @@
 'use Strict';
 
+const Boom = require('boom');
+
 module.exports = (app) => {
   const Profile = app.coincidents.Schemas.profileSchema;
+  const ProfileUtils = app.coincidents.Utils.profileUtils;
+  const Errors = app.coincidents.Utils.errorUtils;
 
-  const updateInfo = (userData) => {
+  const updateInfo = (userData, headers) => {
+    const dictionary = app.coincidents.Translate.gate.selectLanguage(headers.language);
+
+    const updateObject = {};
+    if (userData.name) {
+      updateObject.name = userData.name;
+    }
+    if (userData.description) {
+      updateObject.description = userData.description;
+    }
+    if (userData.newUserName) {
+      updateObject.userName = userData.newUserName;
+    }
+    if (userData.email) {
+      if (ProfileUtils.isEmail(userData.email) === Errors.userErrors.notEmail) {
+        throw Boom.notAcceptable(`${dictionary.notAEmail}.`);
+      }
+      updateObject.email = userData.email;
+      updateObject.confirmedEmail = updateObject.confirmedEmail;
+      //TODO add flag to verify is email was really changed, if yes: call the code to send a confirmation email
+    }
+
     const searchQuery = {
       'userName': userData.userName
     };
     const updateQuery = {
-      '$set': {
-        //TODO Insert here the update fields
-      }
+      '$set': updateObject
     }
 
     return Profile
--- src/services/profileUpdates/profileInfoService.js
+++ src/services/profileUpdates/profileInfoService.js
@@ -1,10 +1,32 @@
 'use Strict';
 
+const Boom = require('boom');
+
 module.exports = (app) => {
   const profileInfoRepository = app.src.repositories.profileUpdates.profileInfoRepository;
+  const Errors = app.coincidents.Utils.errorUtils;
 
-  const updateInfo = (payload, headers) => 
+  const updateInfo = (payload, headers) =>
     profileInfoRepository.updateInfo(payload, headers)
+    .catch((err) => _treatErrors(err, headers.language))
+
+
+  const _treatErrors = (err, language) => {
+    const dictionary = app.coincidents.Translate.gate.selectLanguage(language);
+
+    switch (err.code) {
+      case Errors.mongoErrors._idAlreadyUsed:
+        if (err.message.includes('userName')) {
+          throw Boom.notAcceptable(`${dictionary.userNameAlreadyUsed}.`);
+        }
+        if (err.message.includes('email')) {
+          throw Boom.notAcceptable(`${dictionary.emailAlreadyUsed}.`);
+        }
+        throw Boom.badData(err.message)
+      default:
+        throw Boom.badData(err.message)
+    }
+  }
 
   return {
     updateInfo
