ebfa3b7194c7da728779b63a4612ae733a6ac1ed
Lucas Alves <lucashalves@live.com>
2017-05-18 22:54:03 -0300

    Removing responseInvite service from '/friends'. Because it a '/notification' service

--- src/controllers/friendsController.js
+++ src/controllers/friendsController.js
@@ -3,7 +3,6 @@
 module.exports = (app) => {
   const friendsServices = app.src.services.friends;
   const addFriendService = friendsServices.addFriendService;
-  const responseInviteService = friendsServices.responseInviteService;
 
   const addFriend = (request, reply) => {
     addFriendService.addFriend(request.payload, request.headers)
@@ -13,18 +12,7 @@ module.exports = (app) => {
       .catch((err) => reply(err));
   }
 
-  const responseInvite = (request, reply) => {
-    responseInviteService.responseInvite(request.payload, request.headers)
-      .then((inviteResponse) => {
-        reply(inviteResponse)
-      })
-      .catch((err) => 
-        reply(err)
-      );
-  }
-
   return {
-    addFriend,
-    responseInvite
+    addFriend
   }
 }
--- src/routes/friendsRoutes.js
+++ src/routes/friendsRoutes.js
@@ -33,33 +33,4 @@ module.exports = (app) => {
     }
   })
 
-  server.route({
-    path: '/friends/responseInvite',
-    method: 'PUT',
-    config: {
-      handler: (request, reply) => {
-        friendsController.responseInvite(request, reply)
-      },
-      validate: {
-        payload: Joi.object({
-          userName: Joi.string().required(),
-          invitatorUserName: Joi.string().required(),
-          inviteAccept: Joi.bool().required()
-        }),
-        headers: Joi.object({
-          language: Joi.string().default('en-us')
-        }).unknown()
-      },
-      response: {
-        schema: Joi.object({
-            invitedAnswered: Joi.bool().required(),
-            friendListModified: Joi.bool().required()
-          })
-          .meta({
-            className: 'Response'
-          })
-      }
-    }
-  })
-
 };
