a114d3470c3ae57782e4e0495cd3a72ac3260165
Lucas Alves <lucashalves@live.com>
2017-11-08 20:02:03 -0200

    Fixing teamSchema

--- src/routes/schemas/embeddedSchemas/teamSchema.js
+++ src/routes/schemas/embeddedSchemas/teamSchema.js
@@ -3,11 +3,11 @@
 const Joi = require('joi')
 
 const teamSchema = Joi.object({
-  teamId: Joi.string(),
-  fullName: Joi.string(),
-  shortName: Joi.string(),
+  teamId: Joi.string().required(),
+  fullName: Joi.string().required(),
+  shortName: Joi.string().required(),
   logo: Joi.string(),
-  league: Joi.string()
+  league: Joi.string().required()
 })
 
 module.exports = teamSchema
