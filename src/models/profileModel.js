60addea746e53e322936aca369498f0e84e4577a
Lucas Alves <lucashalves@live.com>
2018-05-27 16:38:54 -0300

    Removing ID from profileModel

--- src/models/profileModel.js
+++ src/models/profileModel.js
@@ -6,106 +6,104 @@ const optionsSchemas = require('./optionsSchemas/optionsSchemas')
 const logoSchema = require('./subValidations/logo')
 const db = require('./connect')
 
-module.exports = (app) => {
-  const Utils = app.coincidents.Utils
-  const Config = app.coincidents.Config
-  const mongo = Config.mongo
+const coincidents = require('iguess-api-coincidents')
 
-  const Schema = mongoose.Schema
-  const serverErrors = Utils.errorUtils.serverErrors
-  const userErrors = Utils.errorUtils.userErrors
+const Utils = coincidents.Utils
+const Config = coincidents.Config
+const mongo = Config.mongo
 
-  const USERNAME_MAX_SIZE = Config.profile.userNameMaxSize
-  const NAME_MAX_SIZE = Config.profile.nameMaxSize
-  const DESCRIPTION_MAX_SIZE = Config.profile.descriptionMaxSize
-  const TEAM_TO_APPRECIATE_MAX_SIZE = Config.profile.maxTeamToAppreciateAllowed
+const Schema = mongoose.Schema
+const serverErrors = Utils.errorUtils.serverErrors
+const userErrors = Utils.errorUtils.userErrors
 
-  const checkUserNameSize = (name) => name.length <= USERNAME_MAX_SIZE
-  const checkNameSize = (name) => name.length <= NAME_MAX_SIZE
-  const checkDescriptionSize = (name) => name.length <= DESCRIPTION_MAX_SIZE
-  const checkAppreciatedTeamsArraySize = (array) => array.length <= TEAM_TO_APPRECIATE_MAX_SIZE
+const USERNAME_MAX_SIZE = Config.profile.userNameMaxSize
+const NAME_MAX_SIZE = Config.profile.nameMaxSize
+const DESCRIPTION_MAX_SIZE = Config.profile.descriptionMaxSize
+const TEAM_TO_APPRECIATE_MAX_SIZE = Config.profile.maxTeamToAppreciateAllowed
 
-  const teamSchema = new Schema({
-    teamRef: {
-      type: String,
-      required: true,
-      validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
-    },
-    league: {
-      type: String,
-      required: true,
-      validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
-    },
-    fullName: {
-      type: String,
-      required: true
-    },
-    shortName: {
-      type: String,
-      required: true
-    },
-    logo: {
-      type: logoSchema
-    }
-  }, optionsSchemas._idAndVersionKeyDisable)
+const checkUserNameSize = (name) => name.length <= USERNAME_MAX_SIZE
+const checkNameSize = (name) => name.length <= NAME_MAX_SIZE
+const checkDescriptionSize = (name) => name.length <= DESCRIPTION_MAX_SIZE
+const checkAppreciatedTeamsArraySize = (array) => array.length <= TEAM_TO_APPRECIATE_MAX_SIZE
 
-  const profileDefinitionSchema = {
-    userName: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: [checkUserNameSize, String(userErrors.userNameSizeExplode)]
-    },
-    name: {
-      type: String,
-      validate: [checkNameSize, String(userErrors.nameSizeExplode)]
-    },
-    avatar: {
-      type: String
-    },
-    footballSupportedTeams: {
-      supportedTeam: teamSchema,
-      appreciatedTeams: {
-        type: [teamSchema],
-        validate: [checkAppreciatedTeamsArraySize, String(userErrors.numberOfAppreciatedTeamsExplode)]
-      }
-    },
-    description: {
-      type: String,
-      validate: [checkDescriptionSize, String(userErrors.descriptionSizeExplode)]
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    confirmedEmail: {
-      type: Boolean
-    },
-    friendList: {
-      type: [{
-        type: String,
-        validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
-      }]
-    },
-    invitedFriendList: {
-      type: [{
-        type: String,
-        validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
-      }]
-    },
-    lastSignInAt: {
-      type: Date
-    }
+const teamSchema = new Schema({
+  teamRef: {
+    type: String,
+    required: true,
+    validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
+  },
+  league: {
+    type: String,
+    required: true,
+    validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  shortName: {
+    type: String,
+    required: true
+  },
+  logo: {
+    type: logoSchema
   }
+}, optionsSchemas._idAndVersionKeyDisable)
 
-  const profileSchema = new Schema(profileDefinitionSchema, optionsSchemas.optionsProfileSchema)
-
-  return db.model('profiles', profileSchema)
+const profileDefinitionSchema = {
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [checkUserNameSize, String(userErrors.userNameSizeExplode)]
+  },
+  name: {
+    type: String,
+    validate: [checkNameSize, String(userErrors.nameSizeExplode)]
+  },
+  avatar: {
+    type: String
+  },
+  footballSupportedTeams: {
+    supportedTeam: teamSchema,
+    appreciatedTeams: {
+      type: [teamSchema],
+      validate: [checkAppreciatedTeamsArraySize, String(userErrors.numberOfAppreciatedTeamsExplode)]
+    }
+  },
+  description: {
+    type: String,
+    validate: [checkDescriptionSize, String(userErrors.descriptionSizeExplode)]
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  confirmedEmail: {
+    type: Boolean
+  },
+  friendList: {
+    type: [{
+      type: String,
+      validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
+    }]
+  },
+  invitedFriendList: {
+    type: [{
+      type: String,
+      validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
+    }]
+  },
+  lastSignInAt: {
+    type: Date
+  }
 }
 
-/*eslint max-statements: 0*/
+const profileSchema = new Schema(profileDefinitionSchema, optionsSchemas.optionsProfileSchema)
+
+module.exports = () => db.model('profiles', profileSchema)
