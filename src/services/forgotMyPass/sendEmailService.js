4b6062c244c0ce365bec101727e2286ab1306f2b
Lucas Alves <lucashalves@live.com>
2018-06-16 16:53:19 -0300

    email html added

--- src/services/forgotMyPass/sendEmailService.js
+++ src/services/forgotMyPass/sendEmailService.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const fs = require('fs')
+const path = require('path')
 const coincidents = require('iguess-api-coincidents')
 const sgMail = require('@sendgrid/mail') //https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
 
@@ -18,7 +20,7 @@ module.exports = () => {
     const dictionary = coincidents.Translate.gate.selectLanguage(headers.language)
     const emailObj = await getProfileByEmailOrUsernameRepository(payload, dictionary)
     const token = await generateToken()()
-    const emailMsg = _buildEmailObj(emailObj, token, dictionary)
+    const emailMsg = await _buildEmailObj(emailObj, token, dictionary)
     
     cacheManager.set(commonData.REDIS_PREFIX_KEY+token, emailObj, TIME_TO_EXPIRE_TOKEN)
     sgMail.send(emailMsg)
@@ -36,19 +38,38 @@ module.exports = () => {
   }
 }
 
-const _buildEmailObj = (emailObj, token, dictionary) => {
+const _buildEmailObj = async (emailObj, token, dictionary) => {
   const emailMsg = {
     to: emailObj.email,
     from: config.sendGrid.emailFrom,
     subject: dictionary.forgotMyPassSubject,
     text: 'z',
-    html: _getEmailHtml(emailObj, token, dictionary)
+    html: await _getEmailHtml(emailObj, token, dictionary)
   }
   
   return emailMsg
 }
 
-const _getEmailHtml = (emailObj, token, dictionary) => 
-  dictionary.forgotMyPassBody
-    .replace('{{token}}', token)
-    .replace('{{userName}}', emailObj.userName)
+const _getEmailHtml = (emailObj, token, dictionary) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, 'html/index.html'), 'utf-8', (err, htmlFile) => {
+      if (err) {
+        return reject(err)
+      }
+      const descriptionWithUserName = dictionary.descriptionPart1.replace('{{userName}}', emailObj.userName)
+  
+      const htmlTranslated = htmlFile
+        .replace('{{titleH1}}', dictionary.titleH1)
+        .replace('{{descriptionPart1}}', descriptionWithUserName)
+        .replace('{{descriptionHighLight}}', dictionary.descriptionHighLight)
+        .replace('{{descriptionPart2}}', dictionary.descriptionPart2)
+        .replace('{{yourTokenIs}}', dictionary.yourTokenIs)
+        .replace('{{afterToken}}', dictionary.afterToken)
+        .replace('{{afterTokenHighLight}}', dictionary.afterTokenHighLight)
+        .replace('{{doubtsBottom}}', dictionary.doubtsBottom)
+        .replace('{{talkWithUsBottom}}', dictionary.talkWithUsBottom)
+        .replace('{{token}}', token)
+  
+      return resolve(htmlTranslated)
+    })
+  })
